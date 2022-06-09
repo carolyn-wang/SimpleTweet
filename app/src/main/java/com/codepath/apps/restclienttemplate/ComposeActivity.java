42fac03a9d9d59433e2fd8f679228c6c185fe4e5
Carolyn Wang <carolynw@fb.com>
2022-06-09 16:14:25 -0700

    Added modal overlay fragment on top of ComposeActivity. Haven't replaced ComposeActivity yet

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -26,56 +27,69 @@ public class ComposeActivity extends AppCompatActivity {
     EditText etCompose;
     Button btnTweet;
     TwitterClient client;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_compose);
+//
+//        client = TwitterApp.getRestClient(this);
+//
+//        etCompose = findViewById(R.id.etCompose);
+//        btnTweet = findViewById(R.id.btnTweet);
+//
+//        // Set click listener on button
+//        btnTweet.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                String tweetContent = etCompose.getText().toString();
+//                if (tweetContent.isEmpty()){
+//                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//                if (tweetContent.length() > MAX_TWEET_LENGTH){
+//                    Toast.makeText(ComposeActivity.this, "Sorry, text is too long", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+//
+//                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+//                    @Override
+//                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+//                        Log.i(TAG, "onSuccess to publish tweet");
+//                        try {
+//                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+//                            Log.i(TAG, "Published tweet says: " + tweet.body);
+//                            Intent intent = new Intent();
+//                            intent.putExtra("tweet", Parcels.wrap(tweet));
+//                            // set result code and bundle data for response
+//                            setResult(RESULT_OK, intent);
+//                            // closes activity, pass data to parent
+//                            finish();
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+//                        Log.e(TAG, "onFailure to publish tweet", throwable);
+//                    }
+//                });
+//            }
+//        });
+//    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        showEditDialog();
+    }
 
-        client = TwitterApp.getRestClient(this);
-
-        etCompose = findViewById(R.id.etCompose);
-        btnTweet = findViewById(R.id.btnTweet);
-
-        // Set click listener on button
-        btnTweet.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                String tweetContent = etCompose.getText().toString();
-                if (tweetContent.isEmpty()){
-                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                if (tweetContent.length() > MAX_TWEET_LENGTH){
-                    Toast.makeText(ComposeActivity.this, "Sorry, text is too long", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
-
-                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Headers headers, JSON json) {
-                        Log.i(TAG, "onSuccess to publish tweet");
-                        try {
-                            Tweet tweet = Tweet.fromJson(json.jsonObject);
-                            Log.i(TAG, "Published tweet says: " + tweet.body);
-                            Intent intent = new Intent();
-                            intent.putExtra("tweet", Parcels.wrap(tweet));
-                            // set result code and bundle data for response
-                            setResult(RESULT_OK, intent);
-                            // closes activity, pass data to parent
-                            finish();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                        Log.e(TAG, "onFailure to publish tweet", throwable);
-                    }
-                });
-            }
-        });
+    private void showEditDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        ComposeFragment composeFragment = ComposeFragment.newInstance("Some Title");
+        composeFragment.show(fm, "fragment_edit_name");
     }
 }
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeFragment.java
@@ -0,0 +1,115 @@
+package com.codepath.apps.restclienttemplate;//package com.codepath.apps.restclienttemplate;
+//
+//import android.os.Bundle;
+//
+//import androidx.fragment.app.Fragment;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link ComposeFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+//public class ComposeFragment extends Fragment {
+//
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+//    // TODO: Rename and change types of parameters
+//    private String mParam1;
+//    private String mParam2;
+//
+//    public ComposeFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment ComposeFragment.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static ComposeFragment newInstance(String param1, String param2) {
+//        ComposeFragment fragment = new ComposeFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_compose, container, false);
+//    }
+//}
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+import org.jetbrains.annotations.Nullable;
+// ...
+
+public class ComposeFragment extends DialogFragment {
+
+    private EditText mEditText;
+
+    public ComposeFragment() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static ComposeFragment newInstance(String title) {
+        ComposeFragment frag = new ComposeFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_compose, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        // Fetch arguments from bundle and set title
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+        // Show soft keyboard automatically and request focus to field
+        mEditText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+}
